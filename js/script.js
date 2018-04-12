ddfd98c1d2988aaf7aeb8cfba3e40f37b76de623
Moaaz <moaaz_bs@yahoo.com>
2018-04-12 15:05:01 +0200

    The whole portfolio

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//const photo = document.querySelector('.photo');
+//const portraitFrontFace = document.querySelector('.portrait-front');
+//const portrait = document.querySelector('.portrait');
+//const scene = document.querySelector('.scene');
+//const workspace = document.querySelector(".workspace");
+//const body = document.body;
+//
+//photo.addEventListener('click', function() {
+//    console.log('Photo');
+//})
+//portraitFrontFace.addEventListener('click', function() {
+//    console.log('portraitFrontFace');
+//})
+//portrait.addEventListener('click', function() {
+//    console.log('portrait');
+//})
+//scene.addEventListener('click', function() {
+//    console.log('scene');
+//})
+//workspace.addEventListener('click', function() {
+//    console.log('workspace');
+//})
+//body.addEventListener('click', function() {
+//    console.log('body');
+//})
+//
